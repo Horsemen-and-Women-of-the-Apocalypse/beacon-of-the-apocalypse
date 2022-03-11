faa66d0bcde67c0fe4290dfe3edc9381182ab464
Thurstag <sashaberthout@hotmail.com>
2022-03-11 21:08:45 +0100

    [NEW] Add the battery level of the flashlight
    Display battery level on screen

--- Assets/Scripts/Common/Flashlight.cs
+++ Assets/Scripts/Common/Flashlight.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,9 +12,39 @@ namespace Common {
     public class ItemsCatchingEvent : UnityEvent<IList<Item>> { }
 
     /// <summary>
+    /// Event describing the remaining percentage of battery
+    /// </summary>
+    [Serializable]
+    public class RemainingBatteryPercentage : UnityEvent<float> { }
+
+    /// <summary>
+    /// Event representing a game over
+    /// </summary>
+    [Serializable]
+    public class GameOverEvent : UnityEvent { }
+
+    /// <summary>
     /// Component managing the collider of the flashlight and sending events about the objects that it detects
     /// </summary>
     public class Flashlight : MonoBehaviour {
+        private const float MaximumBatteryLevel = 100;
+
+        /// <summary>
+        /// Initial level of battery
+        /// </summary>
+        public const float InitialBatteryLevel = MaximumBatteryLevel;
+
+        /// <summary>
+        /// Level of battery considered as critical
+        /// </summary>
+        public const float CriticalBatteryLevel = 25;
+
+        [Tooltip("Frequency of battery level decrease in seconds")]
+        public ushort batteryDecreaseTickFrequency = 10;
+
+        [Tooltip("Percentage of battery to remove per tick")]
+        public float batteryDecreasePerTick = 5;
+
         [Tooltip("Collider representing the flashlight")]
         public CapsuleCollider flashlightCollider;
 
@@ -24,7 +55,12 @@ namespace Common {
 
         public ItemsCatchingEvent onItemCatching;
 
+        public RemainingBatteryPercentage onBatteryDecrease;
+
+        public GameOverEvent onGameOver;
+
         private readonly Dictionary<int, GameObject> _inRangeObjectById = new Dictionary<int, GameObject>();
+        private float _batteryLevel = InitialBatteryLevel;
 
         private void Start() {
             // Adjust collider to fit the light
@@ -33,6 +69,9 @@ namespace Common {
             flashlightCollider.height = lightRange + 1;
             flashlightCollider.radius = Mathf.Tan(Mathf.Deg2Rad * flashlightLight.spotAngle / 2) * lightRange * (spotlightPercentage / 100.0f);
             flashlightCollider.direction = 2;
+
+            // Start battery level decrease
+            StartCoroutine(Consume());
         }
 
         private void OnTriggerEnter(Collider other) {
@@ -83,6 +122,63 @@ namespace Common {
 
             // Notify items caught
             onItemCatching.Invoke(items);
+        }
+
+        /// <summary>
+        /// Decrease the level of the battery by <see cref="batteryDecreasePerTick"/>
+        /// </summary>
+        /// <returns>New battery level</returns>
+        private float Decrease() {
+            var level = _batteryLevel = Mathf.Max(0, _batteryLevel - batteryDecreasePerTick);
+
+            if (level > CriticalBatteryLevel) {
+                return level;
+            }
+
+            // Turn off the light & send game over
+            if (level <= 0) {
+                flashlightLight.intensity = 0;
+                onGameOver.Invoke();
+                return level;
+            }
+
+            // Make the flashlight blink
+            StartCoroutine(Blink());
+            return level;
+        }
+
+        /// <summary>
+        /// Coroutine consuming the battery over time
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator Consume() {
+            while (isActiveAndEnabled && _batteryLevel > 0) {
+                // Wait tick
+                yield return new WaitForSeconds(batteryDecreaseTickFrequency);
+
+                // Decrease and notify battery level
+                onBatteryDecrease.Invoke(Decrease());
+            }
+        }
+
+        /// <summary>
+        /// Coroutine making the flashlight blink
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator Blink() {
+            var intensity = flashlightLight.intensity;
+
+            flashlightLight.intensity = 0;
+            yield return new WaitForSeconds(0.1f);
+
+            flashlightLight.intensity = intensity;
+            yield return new WaitForSeconds(0.1f);
+
+            flashlightLight.intensity = 0;
+            yield return new WaitForSeconds(0.5f);
+
+            // Restore intensity
+            flashlightLight.intensity = intensity;
         }
     }
 

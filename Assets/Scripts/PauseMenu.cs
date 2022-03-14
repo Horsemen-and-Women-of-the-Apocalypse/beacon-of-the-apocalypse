0b0deca6ca866c4454457238819e33a2bfa16cac
Sophie Nguyen <chaussettefurtive@protonmail.com>
2022-03-14 23:37:44 +0100

    [NEW] Make scene fully dark to hide enemies during pause

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,19 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Common;
+using UnityEditor.XR.LegacyInputHelpers;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
     /// <summary>
-    /// Helper flag to get pause state
+    /// Flag to get pause state
     /// </summary>
     public static bool isGamePaused = false;
 
     public GameObject pauseMenuUI;
 
     public SceneChanger sceneChanger;
+    
+    /// <summary>
+    /// Switch on/off existing camera light
+    /// Otherwise, enemies are still visible
+    /// </summary>
+    public Light cameraLight = null;
 
-
+    /// <summary>
+    /// Must be the same as set Skybox
+    /// </summary>
+    public Material skybox;
+    
     /// <summary>
     /// Method to be given to XRRig OnButtonLongPressed event
     /// </summary>
@@ -25,12 +38,19 @@ public class PauseMenu : MonoBehaviour
             Pause();
     }
 
+    private void TurnOnOffCameraLight()
+    {
+        if (cameraLight) cameraLight.enabled = !cameraLight.enabled;
+    }
+
     /// <summary>
     /// Disable pause and resume game
     /// </summary>
     public void Resume()
     {
-        // TODO: Activate torch light
+        TurnOnOffCameraLight();
+        RenderSettings.skybox = skybox;
+        GameObject.Find("Flashlight(Clone)")?.GetComponentInChildren<Flashlight>()?.TurnOnOff();
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isGamePaused = false;
@@ -41,7 +61,9 @@ public class PauseMenu : MonoBehaviour
     /// </summary>
     void Pause()
     {
-        // TODO: Find a way to deactivate torch light
+        TurnOnOffCameraLight();
+        RenderSettings.skybox = (null);
+        GameObject.Find("Flashlight(Clone)")?.GetComponentInChildren<Flashlight>()?.TurnOnOff();
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isGamePaused = true;
